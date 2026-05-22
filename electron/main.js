d507636f5803cb5f2b4039473af3a194d88d7c1a
BhaveshMulchandani <bhaveshmulchandani330@gmail.com>
2026-05-22 23:43:53 +0530

    first commit

---
+++ electron/main.js
@@ -0,0 +1,41 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1400,
+    height: 900,
+
+    minWidth: 800,
+    minHeight: 600,
+
+    autoHideMenuBar: true,
+
+    resizable: true,
+    maximizable: true,
+
+    icon: path.join(__dirname, "../assets/logo.ico"),
+
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  mainWindow.loadURL("http://localhost:5173");
+
+  // mainWindow.webContents.openDevTools();
+}
+
+app.whenReady().then(() => {
+  createWindow();
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
